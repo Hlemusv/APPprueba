740d4f4b45f1863a76253c682b46cf073e44db0d
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-11-23 10:55:09 -0500

    Actualizacion nombre de clases
    Se cambian las clases de InicioAplicacion a LoginInicio

--- app/src/main/java/edu/co/upb/appprueba/LoginInicio.java
+++ app/src/main/java/edu/co/upb/appprueba/LoginInicio.java
@@ -2,13 +2,14 @@ package edu.co.upb.appprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class InicioAplicacion extends AppCompatActivity {
+public class LoginInicio extends AppCompatActivity {
     //Declaracion de un miembro dato de la clase
     private Button btn_login;
     @Override
@@ -22,6 +23,8 @@ public class InicioAplicacion extends AppCompatActivity {
             public void onClick(View v) {
                 //Toast.makeText(InicioAplicacion.this,"se tocó la pantalla",Toast.LENGTH_SHORT);
                 Log.i("upb","mensaje para la universidad");
+                Intent home = new Intent(LoginInicio.this,Home.class); //configuramos la accion del boton desde donde estamos
+                startActivity(home);
             }
         });
     }
