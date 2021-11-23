9a46954023de5a0f0975fef3ca78f32c550a800b
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-11-23 10:31:33 -0500

    Ingreso de layout formulario

--- app/src/main/java/edu/co/upb/appprueba/InicioAplicacion.java
+++ app/src/main/java/edu/co/upb/appprueba/InicioAplicacion.java
@@ -3,12 +3,26 @@ package edu.co.upb.appprueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class InicioAplicacion extends AppCompatActivity {
-
+    //Declaracion de un miembro dato de la clase
+    private Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(InicioAplicacion.this,"se tocó la pantalla",Toast.LENGTH_SHORT);
+                Log.i("upb","mensaje para la universidad");
+            }
+        });
     }
 }
--- app/src/main/java/edu/co/upb/appprueba/Registro.java
+++ app/src/main/java/edu/co/upb/appprueba/Registro.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacto);
+        setContentView(R.layout.activity_registro);
     }
 }
