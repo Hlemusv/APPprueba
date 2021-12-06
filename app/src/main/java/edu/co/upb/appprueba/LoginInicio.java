5db6e777fb917c2f367eb81bbbbc52d72d106b20
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-12-05 19:29:43 -0500

    creacion del registro
    creacion de formulario de registro y botones de interaccion

--- app/src/main/java/edu/co/upb/appprueba/LoginInicio.java
+++ app/src/main/java/edu/co/upb/appprueba/LoginInicio.java
@@ -15,6 +15,7 @@ public class LoginInicio extends AppCompatActivity {
     //Declaracion de un miembro dato de la clase
     private Button btn_login;
     private Datos dato;
+    private Button btn_registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +24,25 @@ public class LoginInicio extends AppCompatActivity {
 
         //le damos identidad encontrando la ID que pusimos en el .xml y le damos vida al boton cuando lo  presionamos
         btn_login = findViewById(R.id.btn_login);
+        //le asignamos el evento setOnClickListener luego new view (para que se llene el @override solo
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //ponemos nuestro codigo como el intent (para solicitar otra pagina), o el Toast.makeText (para aparecer en pantalla un mensaje)
                 Toast.makeText(LoginInicio.this,"se tocó la pantalla boton login",Toast.LENGTH_SHORT).show();
                 Log.i("upb","mensaje que se ve en el log del Run para iniciar sesion");
                 Intent home = new Intent(LoginInicio.this,Home.class); //configuramos la accion del boton desde donde estamos
                 startActivity(home);
                 dato.setNombre("Hamil");
+            }
+
+        });
+        btn_registrar = findViewById(R.id.btn_registrar);
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrar = new Intent(LoginInicio.this,Registro.class);
+                startActivity(registrar);
             }
         });
     }
--- app/src/main/java/edu/co/upb/appprueba/Registro.java
+++ app/src/main/java/edu/co/upb/appprueba/Registro.java
@@ -2,13 +2,24 @@ package edu.co.upb.appprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registro extends AppCompatActivity {
-
+private Button btn_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        btn_main =findViewById(R.id.btn_main);
+        btn_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(Registro.this,Home.class);
+                startActivity(main);
+            }
+        });
     }
 }
